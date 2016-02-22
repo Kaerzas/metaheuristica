2a9f2dea287877e18b83ef109e031e91bda96f09
javierbg <i32bagoj@uco.es>
2016-02-22 14:49:18 +0100

    Implementado SolGeneratorTSP

--- src/problems/tsp/SolGeneratorTSP.java
+++ src/problems/tsp/SolGeneratorTSP.java
@@ -1,14 +1,32 @@
 package problems.tsp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import problems.AbstractSolGenerator;
 import problems.ISolution;
 
 public class SolGeneratorTSP extends AbstractSolGenerator
 {
+	private int nNodes;
+	
+	public SolGeneratorTSP(int nNodes){
+		this.nNodes = nNodes;
+	}
+	
 	@Override
 	public ISolution generate() 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Random randGenerator = new Random(seed);
+		
+		List<Integer> randSol = new ArrayList<Integer>(nNodes);
+		for(int i=0 ; i < nNodes ; ++i){
+			randSol.set(i, i+1);
+		}
+		Collections.shuffle(randSol, randGenerator);
+		
+		return new SolutionTSP(randSol);
 	}
 }
--- src/problems/tsp/SolutionTSP.java
+++ src/problems/tsp/SolutionTSP.java
@@ -1,12 +1,27 @@
 package problems.tsp;
 
+import java.util.List;
+
 import problems.AbstractSolution;
 
 public class SolutionTSP extends AbstractSolution 
 {
+	
+	//private int [] order;
+	private List<Integer> order;
+	
+	public SolutionTSP(List<Integer> order){
+		this.order = order;
+	}
+	
 	@Override
 	public void printSolution() 
 	{
-		// TODO Auto-generated method stub
+		System.out.print("[");
+		for (Integer i : order) {
+			System.out.print(i+",");
+		}
+		System.out.println("]");
+		System.out.println("Fitness:" + getFitness());
 	}
 }
