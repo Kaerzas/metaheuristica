1c7c351e294adb177ad8092cc6e050efd21dfa63
javierbg <javierbg1995@gmail.com>
2016-02-28 21:37:58 +0100

    No. de pruebas configurable en Búsqueda Aleatoria

--- src/metaheuristics/RandomSearch.java
+++ src/metaheuristics/RandomSearch.java
@@ -3,6 +3,8 @@ package metaheuristics;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.configuration.Configuration;
+
 import problems.ISolution;
 
 /**
@@ -14,6 +16,15 @@ import problems.ISolution;
 
 public class RandomSearch extends AbstractAlgorithm 
 {
+	////////////////////////////////////////////////
+	// ---------------------------------- Variables
+	///////////////////////////////////////////////
+	
+	/**
+	 * Number of tries for the Random Search
+	 */
+	private int tries;
+	
 	//////////////////////////////////////////////
 	// ---------------------------------- Methods
 	/////////////////////////////////////////////
@@ -26,7 +37,7 @@ public class RandomSearch extends AbstractAlgorithm
 		ISolution bestSolution = null;
 		
 		// Create 1000 individuals
-		for(int i=0; i<1000; i++) {
+		for(int i=0; i<tries; i++) {
 			
 			// Create new solution and store
 			ISolution newSolution = generator.generate();
@@ -39,10 +50,20 @@ public class RandomSearch extends AbstractAlgorithm
 			if((bestSolution == null) || (instance.betterThan(newSolution, bestSolution))){
 				bestSolution = newSolution;
 				// Show results
-				System.out.println("A best solution has been found in the itration " + i + ":");
+				System.out.println("A best solution has been found in the iteration " + i + ":");
 				bestSolution.printSolution();
 				System.out.println();
 			}
 		}
+	}
+	
+	@Override
+	public void configure(Configuration configuration)
+	{
+		//Standard configuration
+		super.configure(configuration);
+		
+		//Metaheuristic specific configuration
+		this.tries = configuration.getInt("tries");
 	}
 }
