c10bde53e471795dd7224b59792eda76a25fb5ff
aurora <i32estoa@uco.es>
2016-03-15 15:22:31 +0100

    Arregladas dependencias.

--- src/metaheuristics/localsearch/operator/NodeSwapTSP.java
+++ src/metaheuristics/localsearch/operator/NodeSwapTSP.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import problems.ISolution;
-import problems.knapsack.SolutionKnapsack;
 import problems.tsp.SolutionTSP;
 
 public class NodeSwapTSP implements INeighOperator{
