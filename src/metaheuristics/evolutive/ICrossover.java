fdd02c133ea408a57e56f32722f80f3f4861a4a7
javierbg <javierbg1995@gmail.com>
2016-04-25 14:37:11 +0200

    Añadida inicialización en ICrossover

--- src/metaheuristics/evolutive/ICrossover.java
+++ src/metaheuristics/evolutive/ICrossover.java
@@ -4,6 +4,7 @@ import problems.ISolution;
 import util.config.IConfiguration;
 
 public interface ICrossover extends IConfiguration{
-
+	
+	public void initialize(IInstance instance);
 	public ISolution cross(ISolution parentA, ISolution parentB); 
 }
